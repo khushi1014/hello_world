4873cee0b1ff33a9dbb75bd42ed275b76d4daf9c
khushi1014 <130674699+khushi1014@users.noreply.github.com>
2023-06-26 15:27:31 -0400

    Update print message()

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
 print("Hello World!")
 print("\t This is my first Git repository on GitHub.")
+
+def print_message(name='Python'):
+    print(f"Welcome to the class from {name}!)
+          
+name = "Khushi"
+print_message(name)
